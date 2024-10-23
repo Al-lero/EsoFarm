73bcfec8577a838f1372a0b79ad7e3226f6d5355
Al-lero <aleroutieyione@gmail.com>
2024-10-23 15:04:25 +0100

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+const Footer = () =>(
+    <div className="footer-container">
+        <div className="footercontainer1">
+            <img src="https://plus.unsplash.com/premium_photo-1695716658604-b810d4827143?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3BsYXNofGVufDB8fDB8fHww" />
+            <h2>Address : No 20 Folarin Street, Idi Araba, Lagos State</h2>
+            <h2>Contact Person : Mr Richard Arokoyo</h2>
+            <h2>Email:<a href="mailto:richardarokoyo@gmail.com">richardarokoyo@gmail.com</a></h2>
+        </div>
+        <div className="footercontainer2">
+            <h2>About EsoFarm</h2>
+            <h2>Contact Us</h2>
+        </div>
+        <div className="footercontainer3">
+            <h2>About</h2>
+            <h2>Contact Us</h2>
+        </div>
+        <div className="footercontainer4">
+            <h2>About EsoFarm</h2>
+            <h2>Contact Us</h2>
+        </div>
+        <div className="footercontainer5">
+            <h2>About EsoFarm</h2>
+            <h2>Contact Us</h2>
+        </div>
+    </div>
+)
+
+export default Footer
