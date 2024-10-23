6d9e2d54d778a64a078f7e12325400cfec8bedfd
unknown <belloafeez28@gmail.com>
2024-10-23 15:16:38 +0100

    added the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
+import '../components/Style/Header.css'
 const Header = () =>{
 
     return (
         <div className="eso-farm-header">
+            <img src="" alt="" />
+            <h3 className="header-h3">Home</h3>
+            <h3 className="header-h3">Activities</h3>
+            <h3 className="header-h3">Media</h3>
+            <h3 className="header-h3">About Us</h3>
+            <h3 className="header-h3">Contact Us</h3>
+            <button className="btn">Enquiries</button>
+            <div className="chat-with-us">chatWithUs</div>
             </div>
     )
 
