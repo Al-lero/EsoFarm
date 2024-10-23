009c62be42e8e7cacb7c38125b1c17dafb974e1b
unknown <belloafeez28@gmail.com>
2024-10-23 16:12:52 +0100

    added a lil styyle to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,25 @@
+import './Style/Footer.css'
 const Footer = () =>(
-    <div className="footer-container">
-        <div className="footercontainer1">
-            <img src="https://plus.unsplash.com/premium_photo-1695716658604-b810d4827143?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3BsYXNofGVufDB8fDB8fHww" />
+    <div className="footer-Container">
+        <div className="footerContainer">
+            <img src="https://plus.unsplash.com/premium_photo-1695716658604-b810d4827143?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3BsYXNofGVufDB8fDB8fHww" alt='logo' />
             <h2>Address : No 20 Folarin Street, Idi Araba, Lagos State</h2>
             <h2>Contact Person : Mr Richard Arokoyo</h2>
             <h2>Email:<a href="mailto:richardarokoyo@gmail.com">richardarokoyo@gmail.com</a></h2>
         </div>
-        <div className="footercontainer2">
+        <div className="footerContainer">
             <h2>About EsoFarm</h2>
             <h2>Contact Us</h2>
         </div>
-        <div className="footercontainer3">
-            <h2>About</h2>
-            <h2>Contact Us</h2>
-        </div>
-        <div className="footercontainer4">
+        <div className="footerContainer">
             <h2>About EsoFarm</h2>
             <h2>Contact Us</h2>
         </div>
-        <div className="footercontainer5">
+        <div className="footerContainer">
+            <h2>About EsoFarm</h2>
+            <h2>Contact Us</h2>
+        </div>
+        <div className="footerContainer">
             <h2>About EsoFarm</h2>
             <h2>Contact Us</h2>
         </div>
